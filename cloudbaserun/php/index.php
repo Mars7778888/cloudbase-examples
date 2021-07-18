bdb672178f93b24dd310570486d2700914e6033a
Mars7778888 <87607519+Mars7778888@users.noreply.github.com>
2021-07-18 23:24:15 +0800

    Update index.php

--- cloudbaserun/php/index.php
+++ cloudbaserun/php/index.php
@@ -1,2 +1,13 @@
-<?php
-echo sprintf("Hello");
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>随机问答</title>
+</head>
+<body>
+	<div>
+		这是一个问题?
+	</div>
+</body>
+</html>
