34a648e0a4501886038eb35e2fadaaf25ba11d1a
Mars7778888 <87607519+Mars7778888@users.noreply.github.com>
2021-07-19 00:15:02 +0800

    Update index.php

--- cloudbaserun/php/index.php
+++ cloudbaserun/php/index.php
@@ -4,10 +4,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <title>随机问答</title>
+	<style>
+		.body{
+			width:100%;
+			height:100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		.content{
+			width:95%;
+			height:100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+	</style>
 </head>
-<body>
-	<div>
+<body class="body">
+	<div class="content">
 		这是一个问题?
 	</div>
 </body>
 </html>
+
